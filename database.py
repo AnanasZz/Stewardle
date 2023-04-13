7990c2993eaf0e2c12c82e237aa4ced62b2c78da
AnanasZz <84328319+AnanasZz@users.noreply.github.com>
2023-04-13 16:34:47 +0200

    Added database.py with 2023 pilots list with stats as of 13/04/2023

---
+++ database.py
@@ -0,0 +1,23 @@
+pilots={"Verstappen":["VER","0",["RB","TOR"],"33","25","2015","37"],
+        "Perez":["PER","1",["RB","RP","FI"],"11","33","2014","5"],
+        "Alonso":["ALO","2",["AST","ALP","MCL","FER","REN","MIN"],"14","41","2001","32"],
+        "Hamilton":["HAM","3",["MER","MCL"],"44","38","2007","103"],
+        "Sainz":["SAI","2",["FER","MCL","REN","TOR"],"55","28","2015","1"],
+        "Stroll":["STR","4",["AST","RP","WIL"],"18","24","2017","0"],
+        "Russell":["RUS","3",["MER","WIL"],"66","25","2019","1"],
+        "Norris":["NOR","3",["MCL"],"4","23","2019","0"],
+        "Hulkenberg":["HUL","5",["HAA","AST","RP","REN","FI","SAU","WIL"],"27","35","2010","0"],
+        "Leclerc":["LEC","6",["FER","SAU"],"16","25","2018","5"],
+        "Bottas":["BOT","7",["ALF","MER","WIL"],"77","33","2013","10"],
+        "Ocon":["OCO","8",["ALP","REN","RP","FI","MAN"],"31","26","2016","1"],
+        "Piastri":["PIA","9",["MCL"],"81","22","2023","0"],
+        "Gasly":["GAS","8",["ALP","RB","AT","TOR"],"10","27","2017","1"],
+        "Zhou":["ZHO","10",["ALF"],"24","23","2022","0"],
+        "Tsunoda":["TSU","11",["AT"],"22","22","2021","0"],
+        "Magnussen":["MAG","12",["HAA","REN","MCL"],"20","30","2014","0"],
+        "Albon":["ALB","13",["WIL","RB","TOR"],"23","27","2019","0"],
+        "Sargeant":["ZHO","14",["WIL"],"2","22","2023","0"],
+        "De Vries":["DEV","0",["AT"],"21","23","2022","0"],}
+
+# Flag Index
+# 0=netherlands, 1=mexico, 2=spain, 3=uk, 4=canada, 5=germany, 6=monaco, 7=finland, 8=france, 9=australia, 10=china, 11=japan, 12=danemark, 13=thailand, 14=usa
